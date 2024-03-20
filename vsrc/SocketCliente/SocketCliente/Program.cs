cc360b6997285c1c837b460384685c2d6309e95c
Rúben Vieira <110641459+RubenSousaVieira@users.noreply.github.com>
2024-03-20 10:47:41 +0000

    Add files via upload

---
+++ vsrc/SocketCliente/SocketCliente/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace SocketCliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // vamos obter o host e o ip da máquina e especificamos um ponto de ligação
+            IPAddress ipAddress = Dns.GetHostAddresses(Dns.GetHostName())[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, 9000);
+
+            // vamos criar o socket para o cliente
+            Socket cliente = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                cliente.Connect(endPoint);
+
+                // Imprimir a data e hora atual
+                //Console.WriteLine("Data e Hora Atual: " + DateTime.Now);
+
+                // Imprimir o endereço IP do cliente
+                //Console.WriteLine("Endereço IP do Cliente: " + ipAddress.ToString());
+
+                // Obter e imprimir o endereço MAC do cliente
+                NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+                String macAddress = String.Empty;
+                foreach (NetworkInterface adapter in nics)
+                {
+                    if (!adapter.GetPhysicalAddress().ToString().Equals(""))
+                    {
+                        macAddress = adapter.GetPhysicalAddress().ToString();
+                        break;
+                    }
+                }
+                //Console.WriteLine("Endereço MAC do Cliente: " + macAddress); 
+
+                while (true)
+                {
+                    Console.WriteLine("Digite Mensagem: ");
+                    string data = Console.ReadLine();
+                    // vamos enviar em bytes os dados da nossa string data
+                    cliente.Send(Encoding.UTF8.GetBytes(data));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cliente.Close();
+            }
+        }
+    }
+}
---
+++ vsrc/SocketServidor/SocketServidor/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace SocketServidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, 9000);
+            Socket servidor = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                servidor.Bind(endPoint);
+                servidor.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("À espera de ligaçôes...");
+                    Socket socket = servidor.Accept();
+
+                    Thread t1 = new Thread(EscutaCliente);
+                    t1.Start(socket);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                servidor.Close();
+            }
+        }
+
+        static void EscutaCliente(object socket)
+        {
+            Socket s = (Socket)socket;
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                int bytesLenght = s.Receive(bytes);
+                Console.WriteLine("Mensagem: " + Encoding.UTF8.GetString(bytes, 0, bytesLenght));
+
+                string dataHoraAtual = DateTime.Now.ToString();
+                byte[] dataHoraBytes = Encoding.UTF8.GetBytes(dataHoraAtual);
+                s.Send(dataHoraBytes);
+
+                string ipEndereco = ((IPEndPoint)s.LocalEndPoint).Address.ToString();
+                byte[] ipBytes = Encoding.UTF8.GetBytes(ipEndereco);
+                s.Send(ipBytes);
+
+                NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+                string macEndereco = string.Empty;
+                foreach (NetworkInterface adapter in nics)
+                {
+                    if (!adapter.GetPhysicalAddress().ToString().Equals(""))
+                    {
+                        macEndereco = adapter.GetPhysicalAddress().ToString();
+                        break;
+                    }
+                }
+
+                byte[] macBytes = Encoding.UTF8.GetBytes(macEndereco);
+                s.Send(macBytes);
+            }
+        }
+    }
+}
